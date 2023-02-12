755061307b0dfd6139b1b70ac9eb285a225e6a4b
sobuj <sobujahmed124@gmail.com>
2023-02-12 23:30:55 +0600

    added total balance

--- js/deposit.js
+++ js/deposit.js
@@ -3,8 +3,22 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
 // step 2 get the deposit amount from the depsit field
     const depositField = document.getElementById('deposit-field');
     const depositAmount = depositField.value ;
+    
 // step 3 get the current total depsit
     const depositTotalAmount = document.getElementById('deposit-total');
-    depositTotalAmount.innerText = depositAmount;
+    const previousDeposit = depositTotalAmount.innerText;
+    // gettin sum of previous deposit and current deposit
+    const totalDeposit = parseFloat(previousDeposit) + parseFloat(depositAmount);
+    depositTotalAmount.innerText = totalDeposit ;
+
+    // step 4 adding total balance
+    const totalBalance = document.getElementById('total-balance');
+    const previousTotalBalance = totalBalance.innerText;
+    // adding previous and current balance
+    const currentTotalBalance = parseFloat(previousTotalBalance) + parseFloat(depositAmount);
+    // setting current total balance
+    totalBalance.innerText = currentTotalBalance;
     
+    // clearing the input field
+    depositField.value = '';
 })
